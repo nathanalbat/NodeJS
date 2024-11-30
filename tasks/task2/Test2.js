5c42b53a4eee403267e8c6c4fdb871b7d9e3b118
nathanalbat <80818365+nathanalbat@users.noreply.github.com>
2024-11-30 21:57:10 +0200

    Add files via upload

---
+++ tasks/task2/Test2.js
@@ -0,0 +1,11 @@
+//Task 2
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) if (num % i === 0) return false;
+  return true;
+}
+for (let i = 2; i < 237; i++) {
+  if (isPrime(i)) {
+    console.log(i);
+  }
+}
